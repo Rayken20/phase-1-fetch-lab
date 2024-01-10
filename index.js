0c3f853397f9c219887fd731dd2db01f25694311
Rachael <rachael92njoki@gmail.com>
2024-01-10 15:15:53 +0300

    complete

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 function fetchBooks() {
-  return fetch("https://anapioficeandfire.com/api/books")
-    .then(resp => resp.json())
-    .then(json => renderBooks(json))
+  // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((res) => {return res.json()})
+  .then((data) => renderBooks(data));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main')
+  const main = document.querySelector('main');
   books.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
-    main.appendChild(h2)
-  })
+    const h2 = document.createElement('h2');
+    h2.innerHTML = book.name;
+    main.appendChild(h2);
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+  fetchBooks();
+});
